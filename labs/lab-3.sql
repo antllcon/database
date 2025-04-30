7b1c6fc9ee2d4996bcd49970dfe0f1b67b462d6a
antllcon <pokemonivan32@gmail.com>
2025-04-30 10:22:03 +0300

    Реализация 1 задания из 3 лабы

--- labs/lab-3.sql
+++ labs/lab-3.sql
@@ -5,30 +5,10 @@ CREATE TABLE IF NOT EXISTS `news`
 (
     `id`           INT UNSIGNED AUTO_INCREMENT,
     `name`         VARCHAR(255)        NOT NULL,
+    `image_id`     BINARY(16)          NOT NULL,
+    `author_id`    INT UNSIGNED        NOT NULL,
     `is_published` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`)
-)
-    ENGINE = InnoDB
-    CHARACTER SET = utf8mb4
-    COLLATE utf8mb4_unicode_ci
-;
-
-CREATE TABLE IF NOT EXISTS `text_block`
-(
-    `block_id` BINARY(16) NOT NULL,
-    `text`     TEXT       NOT NULL,
-    PRIMARY KEY (`block_id`)
-)
-    ENGINE = InnoDB
-    CHARACTER SET = utf8mb4
-    COLLATE utf8mb4_unicode_ci
-;
-
-CREATE TABLE IF NOT EXISTS `image_block`
-(
-    `block_id` BINARY(16) NOT NULL,
-    `image`    LONGBLOB   NOT NULL,
-    PRIMARY KEY (`block_id`)
 )
     ENGINE = InnoDB
     CHARACTER SET = utf8mb4
@@ -43,6 +23,30 @@ CREATE TABLE IF NOT EXISTS `news_block`
     PRIMARY KEY (`block_id`),
     UNIQUE (`news_id`, `position`),
     FOREIGN KEY (`news_id`) REFERENCES `news` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+)
+    ENGINE = InnoDB
+    CHARACTER SET = utf8mb4
+    COLLATE utf8mb4_unicode_ci
+;
+
+CREATE TABLE IF NOT EXISTS `text_block`
+(
+    `block_id` BINARY(16) NOT NULL,
+    `text`     TEXT       NOT NULL,
+    PRIMARY KEY (`block_id`),
+    FOREIGN KEY (`block_id`) REFERENCES `news_block` (`block_id`) ON DELETE CASCADE ON UPDATE CASCADE
+)
+    ENGINE = InnoDB
+    CHARACTER SET = utf8mb4
+    COLLATE utf8mb4_unicode_ci
+;
+
+CREATE TABLE IF NOT EXISTS `image_block`
+(
+    `block_id` BINARY(16) NOT NULL,
+    `image`    LONGBLOB   NOT NULL,
+    PRIMARY KEY (`block_id`),
+    FOREIGN KEY (`block_id`) REFERENCES `news_block` (`block_id`) ON DELETE CASCADE ON UPDATE CASCADE
 )
     ENGINE = InnoDB
     CHARACTER SET = utf8mb4
@@ -80,16 +84,53 @@ CREATE TABLE IF NOT EXISTS `comment`
     COLLATE utf8mb4_unicode_ci
 ;
 
-INSERT INTO `news` (name, is_published)
-VALUES ('Top 10 goobie woobies', TRUE),
-       ('Italian animals', FALSE),
-       ('How to make Database?', FALSE),
-       ('Nice memes', TRUE),
-       ('Why I live at weekend?', TRUE);
+CREATE TABLE `author`
+(
+    `author_id`  INT UNSIGNED AUTO_INCREMENT,
+    `name`       VARCHAR(255)        NOT NULL,
+    `email`      VARCHAR(100)        NOT NULL UNIQUE COMMENT "Как хранить email?",
+    `bio`        TEXT                NULL,
+    `created_at` TIMESTAMP           NOT NULL DEFAULT NOW(),
+    `is_active`  TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+    PRIMARY KEY (`author_id`)
+)
+    ENGINE = InnoDB
+    CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci
+;
+
+ALTER TABLE `news`
+    ADD FOREIGN KEY (`author_id`) REFERENCES `author` (`author_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+CREATE TABLE `thread`
+(
+    `thread_id`   VARCHAR(255) NOT NULL,
+    `title`       VARCHAR(255) NOT NULL,
+    `description` TEXT         NULL,
+    `created_by`  INT UNSIGNED NULL,
+    `created_at`  TIMESTAMP    NOT NULL DEFAULT NOW(),
+    `is_locked`   TINYINT(1)   NOT NULL DEFAULT 0,
+    PRIMARY KEY (`thread_id`),
+    FOREIGN KEY (`created_by`) REFERENCES `author` (`author_id`) ON DELETE SET NULL ON UPDATE CASCADE
+)
+    ENGINE = InnoDB
+    CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci
+;
+
+ALTER TABLE `author`
+    ADD COLUMN `last_login` DATETIME                NULL AFTER `is_active`,
+    ADD COLUMN `avatar`     LONGBLOB                NULL,
+    ADD COLUMN `gender`     ENUM ('male', 'female') NULL DEFAULT NULL AFTER `avatar`
+;
+
+ALTER TABLE `thread`
+    ADD COLUMN `is_visible` TINYINT(1)   NOT NULL DEFAULT 1 AFTER `is_locked`,
+    ADD COLUMN `color_code` VARCHAR(255) NOT NULL DEFAULT '#FFFFFF' AFTER `description`,
+    ADD COLUMN `is_starred` TINYINT(1)   NOT NULL DEFAULT 0
+;
 
 SELECT *
 FROM `news_block`;
 
 DROP DATABASE `news_feed`;
-
-
